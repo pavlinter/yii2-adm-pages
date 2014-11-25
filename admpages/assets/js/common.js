3335938c15bef8688b7d07ee93bb9a242297b97f
Pavel <pavlinter@gmail.com>
2014-11-25 17:57:58 +0200

    .

--- admpages/assets/js/common.js
+++ admpages/assets/js/common.js
@@ -1,18 +1,18 @@
 (function($){
     $(function(){
-        $(document).on('click','.btn-change-to-alias,.btn-change-to-link', function(){
+        $(document).on("click",".btn-change-to-alias,.btn-change-to-link", function(){
             var $el = $(this);
-            var $cont = $el.closest('.admpage-alias-cont');
-            var $contAlias  = $('.btn-change-to-link', $cont).closest('.form-group');
-            var $contUrl    = $('.btn-change-to-alias', $cont).closest('.form-group');
-            if($el.is('.btn-change-to-link')){
-                $contUrl.removeClass('hide');
-                $contAlias.addClass('hide');
-                $contAlias.find(':input').val('');
+            var $cont = $el.closest(".admpage-alias-cont");
+            var $contAlias  = $(".btn-change-to-link", $cont).closest(".form-group");
+            var $contUrl    = $(".btn-change-to-alias", $cont).closest(".form-group");
+            if($el.is(".btn-change-to-link")){
+                $contUrl.removeClass("hide");
+                $contAlias.addClass("hide");
+                $contAlias.find(":input").val("");
             } else {
-                $contAlias.removeClass('hide');
-                $contUrl.find(':input').select2('val', '');
-                $contUrl.addClass('hide');
+                $contAlias.removeClass("hide");
+                $contUrl.find(":input").select2("val", "");
+                $contUrl.addClass("hide");
             }
         });
     });
